e836025f8fecc3b0794de3f7701e8e4167ba0c03
Younes Ben Yahia <younes.ben-yahia@universite-paris-saclay.fr>
2026-05-04 16:33:32 +0200

    la fonction perimeter

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -2,3 +2,12 @@
 #include "shapes/triangle.hpp"
 
 Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {}
+
+double Triangle::perimeter() {
+
+	double d1 = A.distance(B);
+	double d2 = B.distance(C);
+	double d3 = C.distance(A);
+
+	return d1 + d2 + d3;
+}
