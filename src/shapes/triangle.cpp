3616be7d45fc3f37244abef6bcccadd7090d2b95
Younes ben yahia <younes.ben-yahia@universite-paris-saclay.fr>
2026-05-09 22:33:52 +0200

    fonction perimeter du triangle modifier

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -3,11 +3,12 @@
 
 Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {}
 
+
 double Triangle::perimeter() {
 
 	double d1 = A.distance(B);
 	double d2 = B.distance(C);
 	double d3 = C.distance(A);
 
-	return d1 + d2 + d3;
+	return d1 + d2 + d3; //on somme les 3 cotés pour obtenir le perimetre
 }
