5cbf8b2377f1b9f9020c58ed4b2a2eb42ca1eb08
Younes ben yahia <younes.ben-yahia@universite-paris-saclay.fr>
2026-05-10 18:31:15 +0200

    fonction air triangle

--- src/shapes/triangle.cpp
+++ src/shapes/triangle.cpp
@@ -1,5 +1,7 @@
 #include "point.hpp"
 #include "shapes/triangle.hpp"
+#include <cmath>
+
 
 Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {}
 
@@ -11,4 +13,15 @@ double Triangle::perimeter() {
 	double d3 = C.distance(A);
 
 	return d1 + d2 + d3; //on somme les 3 cotés pour obtenir le perimetre
+}
+
+double Triangle::area(){
+
+	double d1 = A.distance(B);
+	double d2 = B.distance(C);
+	double d3 = C.distance(A);
+	//on cacule de demie perimetre
+	double p= perimeter()/2;
+
+	return std::sqrt(p*(p*d1)+(p*d3)+(p*d2)); //la formule de Héron 
 }
